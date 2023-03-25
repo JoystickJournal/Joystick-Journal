25541038eeb5b260f6b0b72ec1ca2416a0624c30
Knox-Phillips <knox-phillips@citypolyhs.org>
2023-03-25 01:22:49 -0400

    Checking final pushed for the night

--- Categories/top-games.js
+++ Categories/top-games.js
@@ -7,10 +7,44 @@ document.addEventListener('DOMContentLoaded',function(){
 }
 )
 
-
-
-/*
-<!-- <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-    Launch demo modal
-  </button> -->
-*/
+// Define a function to make the API request and cache the data in local storage
+function fetchGame(gameId) {
+    // Check if the game is already stored in the local storage
+    const cachedGames = JSON.parse(localStorage.getItem('games')) || {};
+    if (cachedGames[gameId]) {
+      // If the game is already stored in the local storage, return it
+      console.log(`Retrieving game ${gameId} from cache`);
+      return Promise.resolve(cachedGames[gameId]);
+    } else {
+      // If the game is not stored in the local storage, make the API request
+      const url = `https://api.rawg.io/api/games/${gameId}`;
+      return fetch(url)
+        .then(response => response.json())
+        .then(game => {
+          // Store the game in the local storage
+          console.log(`Caching game ${gameId} in local storage`);
+          cachedGames[gameId] = game;
+          localStorage.setItem('games', JSON.stringify(cachedGames));
+          // Return the game data
+          return game;
+        });
+    }
+  }
+  
+  // Call the fetchGame function to make the API request and get the game data
+  fetchGame(3498).then(game => {
+    console.log(`Game ${game.id}: ${game.name}`);
+    // Do something with the game data
+  });
+  
+  fetchGame(3328).then(game => {
+    console.log(`Game ${game.id}: ${game.name}`);
+    // Do something with the game data
+  });
+  
+  fetchGame(4200).then(game => {
+    console.log(`Game ${game.id}: ${game.name}`);
+    // Do something with the game data
+  });
+  
+  console.log(localStorage.getItem('games'));

e033650f2a4f77a772a1b2a9c990f1a322afb4e5
Knox-Phillips <knox-phillips@citypolyhs.org>
2023-03-26 01:33:28 -0400

    Pushing up my code so far for fetching the games images and description onto a second page

---
+++ Categories/Second-Page/secondPage.js
@@ -0,0 +1,54 @@
+const cardContainer = document.querySelector('section')
+
+const genreName = localStorage.getItem('genreName').toLowerCase();
+
+console.log(genreName)
+
+let countID = 0
+
+const fetchGameData = async (id) => {
+  const cacheKey = `game-${id}`;
+  const cachedData = localStorage.getItem(cacheKey);
+
+  if (cachedData) {
+    console.log(`Using cached data for game ${id}`);
+    return JSON.parse(cachedData);
+  }
+
+  const response = await fetch(`https://api.rawg.io/api/games/${id}?key=${config.api}`);
+  const data = await response.json();
+
+  localStorage.setItem(cacheKey, JSON.stringify(data));
+
+  console.log(`Fetched data for game ${id}`);
+
+  return data;
+};
+
+const renderCard = async (game) => {
+  const { name, background_image, id } = game;
+
+  const card = document.createElement('div');
+  card.innerHTML = `<div class="card mb-3">
+  <img src="${background_image}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${name}</h5>
+    <p class="card-text">${game.description || 'Loading...'}</p>
+  </div>
+</div>`;
+
+
+  cardContainer.append(card);
+
+  const gameData = await fetchGameData(id);
+  const { description } = gameData;
+  card.querySelector('.card-text').innerHTML = description;
+  count++
+};
+
+fetch(`https://api.rawg.io/api/games?genres=${genreName}&key=` + config.api)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    data.results.forEach(renderCard);
+  });
--- Categories/top-games.js
+++ Categories/top-games.js
@@ -1,4 +1,4 @@
-
+window.carryOverGenre
 
 let count = 10
 
@@ -72,17 +72,22 @@ function fetchGame(gameId) {
 
   //Search Bar
 
-  document.getElementById('searchBar').addEventListener('click', async (e) => {
-    const body = document.querySelector(".list")
-    e.preventDefault()
-    body.innerHTML=''
-    const input = document.querySelector('input').value
-    await searchGames(input).then(data => {
-      data.forEach(async (elem) => {
-        const listElement = document.createElement('li')
-        listElement.style.display = "flex"
-        listElement.style.justifyContent ="center"
-        listElement.innerHTML = `<div class="mb-5 mx-5 h-100 w-100">
+// Search Bar
+
+function handleSearchBar(visible) {
+  const body = document.querySelector('.list');
+  const input = document.querySelector('input').value;
+
+  if (!visible) {
+    body.innerHTML = '';
+  }
+
+  searchGames(input).then((data) => {
+    data.forEach((elem) => {
+      const listElement = document.createElement('li');
+      listElement.style.display = 'flex';
+      listElement.style.justifyContent = 'center';
+      listElement.innerHTML = `<div class="mb-5 mx-5 h-100 w-100">
         <div class="card h-100 w-100">
           <!-- Product details-->
           <div class="card-body " style="z-index: 1;">
@@ -97,107 +102,216 @@ function fetchGame(gameId) {
             <div class="text-center mt-5"><a class="btn btn-dark mt-auto" data-bs-toggle="collapse" href="#collapseExample${count}" role="button" aria-expanded="false" aria-controls="collapseExample"><p class="text-light my-0">Definition</p></a></div>
             <div class="collapse text-center"  id="collapseExample${count}"  style="z-index: 1;">
               <p class="card-text fw-bold text-dark"></p>
-              <a href="#" id="cardButton" class="btn btn-primary">View More</a>
+              <a href="#" id="cardButton${count}" class="btn btn-primary">View More</a>
             </div>
           </div>
           <!-- Product image-->
           <div class="card-img-overlay" style="background-image: url('${elem.background_image}'); opacity: 1; pointer-events: none; background-size: cover;"></div>
         </div>
-      </div>`
+      </div>`;
 
-        body.append(listElement)
-        count+=1
-      })
-    })
+      body.append(listElement);
 
-  })
+      if (visible) {
+        document
+          .querySelector('#cardButton' + count)
+          .addEventListener('click', () => {
+            const modal = document.querySelector('#myModal');
+            const modalBody = document.querySelector('.modal-body');
 
+            const title = document.createElement('h2');
+            title.textContent = elem.name;
+
+            const rating = document.createElement('p');
+            rating.textContent = `Rating: ${elem.rating}`;
+
+            const description = document.createElement('p');
+            description.textContent = elem.description;
+
+            // Clear the modal body and add the game data elements
+            modalBody.innerHTML = '';
+            modalBody.appendChild(title);
+            modalBody.appendChild(rating);
+            modalBody.appendChild(description);
+
+            // Show the modal
+            modal.show();
+          });
+      }
+
+      count += 1;
+    });
+  });
+}
+
+document.getElementById('searchBar').addEventListener('click', (e) => {
+  e.preventDefault();
+
+  handleSearchBar(true);
+});
+
+handleSearchBar(false);
+
+// View more buttons
+const viewMoreButtons = document.querySelectorAll('.btn-primary');
+
+// Search bar
+const searchBar = document.getElementById('searchBar');
+
+// Modal
+const modal = document.querySelector('#myModal');
+const modalBody = document.querySelector('.modal-body');
+
+// Function to fetch game data and display modal
+const displayGameDetails = async (gameId) => {
+  try {
+    // Fetch data for the selected game
+    const response = await fetch(`https://api.rawg.io/api/games/${gameId}?key=${config.api}`);
+    const data = await response.json();
+
+    // Create elements to display the game data
+    const title = document.createElement('h2');
+    title.textContent = data.name;
+
+    const rating = document.createElement('p');
+    rating.textContent = `Rating: ${data.rating}`;
+
+    const description = document.createElement('p');
+    description.textContent = data.description;
+
+    // Clear the modal body and add the game data elements
+    modalBody.innerHTML = '';
+    modalBody.appendChild(title);
+    modalBody.appendChild(rating);
+    modalBody.appendChild(description);
+
+    // Show the modal
+    modal.show();
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
+
+// Event listener for view more buttons
+viewMoreButtons.forEach((button) => {
+  button.addEventListener('click', async (e) => {
+    e.preventDefault();
+    console.log('hello')
+    // Disable search bar functionality
+    searchBar.removeEventListener('click', searchHandler);
+
+    // Get game ID from button ID
+    const gameId = button.id.slice(10);
+
+    // Display game details in modal
+    await displayGameDetails(gameId);
+
+    // Re-enable search bar functionality when modal is closed
+    modal.addEventListener('hidden.bs.modal', () => {
+      searchBar.addEventListener('click', searchHandler);
+    });
+  });
+});
+  
 
   const genresContainer = document.querySelector('#genres');
   const gameDataContainer = document.querySelector('#game-data');
   
-  // Fetch genres from the RAWG API and display them
-  fetch('https://api.rawg.io/api/genres?key=' + config.api)
-    .then(response => response.json())
-    .then(data => {
-      data.results.forEach(genre => {
-        // Create a new element for the genre card
-        const card = document.createElement('div');
-        card.classList.add('card');
-        card.style.backgroundImage = `url(${genre.image_background})`;
-        card.style.backgroundPosition = "center"
-        card.style.backgroundSize= "cover"
-        card.style.backgroundRepeat ="no-repeat"
-        card.style.width="300px"
-        card.style.height="300px"
+ // Fetch genres from the RAWG API and display them
+fetch('https://api.rawg.io/api/genres?key=' + config.api)
+.then(response => response.json())
+.then(data => {
+  data.results.forEach(genre => {
+    // Create a new element for the genre card
+    const card = document.createElement('div');
+    card.classList.add('card', 'mb-5'); // Add 'mb-5' class for margin
+    card.style.backgroundImage = `url(${genre.image_background})`;
+    card.style.backgroundPosition = "center"
+    card.style.backgroundSize= "cover"
+    card.style.backgroundRepeat ="no-repeat"
+    card.style.width="300px"
+    card.style.height="300px"
 
-        card.setAttribute('id', 'hoverCard')
-  
-        // Create the card body
-        const cardBody = document.createElement('div');
-        cardBody.classList.add('card-body');
-        const cardText = document.createElement('p')
+    card.setAttribute('id', 'hoverCard')
 
-        cardText.setAttribute('class','card-text')
+    // Create the card body
+    const cardBody = document.createElement('div');
+    cardBody.classList.add('card-body');
 
-        cardText.textContent = genre.games[0].name
 
-        // Create the card title
-        const cardTitle = document.createElement('h5');
-        cardTitle.classList.add('card-title', 'text-light');
-        cardTitle.textContent = genre.name;
-        cardBody.appendChild(cardTitle);
-        cardBody.style.display="flex"
-        cardBody.style.justifyContent="center"
-  
-        // Add the card body to the card
-        card.appendChild(cardBody);
-  
-        // Add event listener to each genre card
-        card.addEventListener('click', () => {
-          // Check if game data is already in local storage
-          const cachedGameData = JSON.parse(localStorage.getItem(genre.slug));
-          if (cachedGameData) {
-            // Display game data from local storage
-            displayGameData(cachedGameData);
-          } else {
-            // Fetch game data for the selected genre
-            fetch(`https://api.rawg.io/api/games?genres=${genre.slug}&ordering=-rating&key=` + config.api)
-              .then(response => response.json())
-              .then(data => {
-                // Cache game data in local storage
-                localStorage.setItem(genre.slug, JSON.stringify(data.results));
-  
-                // Display game data
-                displayGameData(data.results);
-              })
-              .catch(error => {
-                console.error('Error fetching data:', error);
-              });
-          }
-        });
-  
-        // Add the genre card to the container
-        const col = document.createElement('div');
-        col.classList.add('col', 'mb-5');
-        col.appendChild(card);
-        genresContainer.appendChild(col);
+    // Create the card title
+    const cardTitle = document.createElement('h5');
+    cardTitle.classList.add('card-title', 'text-light', 'fw-bolder');
+    cardTitle.textContent = genre.name;
+    cardBody.appendChild(cardTitle);
+    cardBody.style.display="flex"
+    cardBody.style.justifyContent="center"
+
+    // Add the card body to the card
+    card.appendChild(cardBody);
+    console.log(genre.name)
+    // Add event listener to each genre card
+// Add event listener to each genre card
+card.addEventListener('click', () => {
+  localStorage.setItem('genreName', genre.name);
+  card.style.width="100%"
+  card.style.height="100%"
+  // Check if game data is already in local storage
+  const cachedGameData = JSON.parse(localStorage.getItem(genre.slug));
+  if (cachedGameData) {
+    // Display game data from local storage
+    window.location.href= "Second-page/second-page.html"
+
+    displayGameData(cachedGameData);
+  } else {
+    // Fetch game data for the selected genre
+    fetch(`https://api.rawg.io/api/games?genres=${genre.slug}&ordering=-rating&key=` + config.api)
+      .then(response => response.json())
+      .then(data => {
+        // Cache game data in local storage
+        localStorage.setItem(genre.slug, JSON.stringify(data.results));
+        window.location.href= "Second-page/second-page.html"
+
+        // Display game data
+        displayGameData(data.results);
+
+        // Add the "expanded" class to the game data container after displaying the data
+        gameDataContainer.classList.add('w-100'); // Change class to w-100
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
       });
+  }
+
+});
+    
+
+
+    // Add the genre card to the container
+    const col = document.createElement('div');
+    col.style.width="auto"
+    col.appendChild(card); // Remove 'col' class from parent element
+    col.addEventListener('click', function(){
+
     })
-    .catch(error => {
-      console.error('Error fetching data:', error);
-    });
+    genresContainer.appendChild(card);
+  });
+})
+.catch(error => {
+  console.error('Error fetching data:', error);
+});
+
   
   // Function to display game data
   function displayGameData(data) {
     // Create an element to display the game data
     const gameData = document.createElement('div');
-  
+    gameData.style.width="auto"
     // Add the game data to the element
     data.forEach(game => {
       const p = document.createElement('p');
       p.textContent = game.name;
-      p.style.color = "white"
+      p.style.color = "black"
       gameData.appendChild(p);
     });
   
